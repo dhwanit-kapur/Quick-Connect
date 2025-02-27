14a3ae8eccb7eaf22597f8fadc84fe122b6f4dc7
Dhwanit <dhwanitkapur.dk@gmail.com>
2025-02-27 06:32:42 +0530

    ADD: Setup auth using clerk

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+
 import "./globals.css";
+
+import { ClerkProvider } from "@clerk/nextjs";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -17,10 +20,26 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${inter.className}  antialiased bg-dark-2`}>
-        {children}
-      </body>
-    </html>
+    <ClerkProvider
+      appearance={{
+        layout: {
+          logoImageUrl: "/icons/app-logo.svg",
+          socialButtonsVariant: "iconButton",
+        },
+        variables: {
+          colorText: "#fff",
+          colorPrimary: "#0E78F9",
+          colorBackground: "#1c1f2e",
+          colorInputBackground: "#252a41",
+          colorInputText: "#fff",
+        },
+      }}
+    >
+      <html lang="en">
+        <body className={`${inter.className}  antialiased bg-dark-2`}>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,6 +2,8 @@ import Image from "next/image";
 import Link from "next/link";
 import MobileNav from "./MobileNav";
 
+import { SignedIn, UserButton } from "@clerk/nextjs";
+
 export default function Navbar() {
   return (
     <nav className="flex-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10">
@@ -19,7 +21,9 @@ export default function Navbar() {
       </Link>
 
       <div className="flex-between gap-5">
-        {/* Clerk - User Management  */}
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
 
         <MobileNav />
       </div>
