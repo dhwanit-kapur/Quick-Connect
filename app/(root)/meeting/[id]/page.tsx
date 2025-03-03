3fcf725b0abb073f8a0f6c334ca702e5ea35bbab
Dhwanit <dhwanitkapur.dk@gmail.com>
2025-03-03 17:53:46 +0530

    ADD: Custom getCallById hook

--- app/(root)/meeting/[id]/page.tsx
+++ app/(root)/meeting/[id]/page.tsx
@@ -1,12 +1,33 @@
+"use client";
+
+import { useState } from "react";
+
+import { useUser } from "@clerk/nextjs";
+import { StreamCall, StreamTheme } from "@stream-io/video-react-sdk";
+import MeetingRoom from "@/components/MeetingRoom";
+import MeetingSetup from "@/components/MeetingSetup";
+import { useGetCallById } from "@/hooks/useGetCallById";
+import Loader from "@/components/Loader";
+
 export default async function MeetingPage({
-  params,
+  params: { id },
 }: {
   params: { id: string };
 }) {
-  const { id } = await params;
+  const { user, isLoaded } = useUser();
+  const [isSetupComplete, setIsSetupComplete] = useState(false);
+
+  const { call, isCallLoading } = useGetCallById(id);
+
+  if (!isLoaded || isCallLoading) return <Loader />;
+
   return (
-    <div>
-      <p>Meeting id: #{id}</p>
-    </div>
+    <main>
+      <StreamCall call={call}>
+        <StreamTheme>
+          {isSetupComplete ? <MeetingRoom /> : <MeetingSetup />}
+        </StreamTheme>
+      </StreamCall>
+    </main>
   );
 }
--- components/ui/sonner.tsx
+++ components/ui/sonner.tsx
@@ -15,7 +15,7 @@ const Toaster = ({ ...props }: ToasterProps) => {
       toastOptions={{
         classNames: {
           toast:
-            "group toast group-[.toaster]:bg-background group-[.toaster]:text-foreground group-[.toaster]:border-border group-[.toaster]:shadow-lg",
+            "group toast group-[.toaster]:shadow-lg bg-dark-1 text-white !border-none",
           description: "group-[.toast]:text-muted-foreground",
           actionButton:
             "group-[.toast]:bg-primary group-[.toast]:text-primary-foreground",
