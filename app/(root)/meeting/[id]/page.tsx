317fa2943368356055976d60f2fabeadc2f5f695
Dhwanit <dhwanitkapur.dk@gmail.com>
2025-03-03 18:52:18 +0530

    ADD: Meeting Setup component

--- app/(root)/meeting/[id]/page.tsx
+++ app/(root)/meeting/[id]/page.tsx
@@ -8,16 +8,17 @@ import MeetingRoom from "@/components/MeetingRoom";
 import MeetingSetup from "@/components/MeetingSetup";
 import { useGetCallById } from "@/hooks/useGetCallById";
 import Loader from "@/components/Loader";
+import { useParams } from "next/navigation";
 
-export default async function MeetingPage({
-  params: { id },
-}: {
-  params: { id: string };
-}) {
+export default function MeetingPage() {
+  const params = useParams();
+
+  if (!params.id) return <Loader />;
+
   const { user, isLoaded } = useUser();
   const [isSetupComplete, setIsSetupComplete] = useState(false);
 
-  const { call, isCallLoading } = useGetCallById(id);
+  const { call, isCallLoading } = useGetCallById(params.id);
 
   if (!isLoaded || isCallLoading) return <Loader />;
 
@@ -25,7 +26,11 @@ export default async function MeetingPage({
     <main>
       <StreamCall call={call}>
         <StreamTheme>
-          {isSetupComplete ? <MeetingRoom /> : <MeetingSetup />}
+          {isSetupComplete ? (
+            <MeetingRoom />
+          ) : (
+            <MeetingSetup setIsSetupComplete={setIsSetupComplete} />
+          )}
         </StreamTheme>
       </StreamCall>
     </main>
--- components/MeetingSetup.tsx
+++ components/MeetingSetup.tsx
@@ -1,3 +1,62 @@
-export default function MeetingSetup() {
-  return <div>Meeting Setup</div>;
+"use client";
+
+import { useEffect, useState } from "react";
+
+import {
+  DeviceSettings,
+  useCall,
+  VideoPreview,
+} from "@stream-io/video-react-sdk";
+import { Button } from "./ui/button";
+
+export default function MeetingSetup({
+  setIsSetupComplete,
+}: {
+  setIsSetupComplete: (value: boolean) => void;
+}) {
+  const [isMicCamToggledOn, setIsMicCamToggledOn] = useState<boolean>(false);
+
+  const call = useCall();
+
+  if (!call)
+    throw new Error(
+      "useCall hook must be used inside the Stream Call component"
+    );
+
+  useEffect(() => {
+    if (isMicCamToggledOn) {
+      call?.microphone.disable();
+      call?.camera.disable();
+    } else {
+      call?.microphone.enable();
+      call?.camera.enable();
+    }
+  }, [isMicCamToggledOn, call?.microphone, call?.camera]);
+
+  return (
+    <div className="flex flex-col h-screen w-full gap-3 items-center justify-center text-white">
+      <h1 className="text-2xl font-bold">Setup</h1>
+      <VideoPreview />
+      <div className="flex h-16 items-center justify-center gap-3">
+        <label className="flex items-center justify-center gap-2 font-medium">
+          <input
+            type="checkbox"
+            checked={isMicCamToggledOn}
+            onChange={(e) => setIsMicCamToggledOn(e.target.checked)}
+          />
+          Join with mic and camera off
+        </label>
+        <DeviceSettings />
+      </div>
+      <Button
+        className="rounded-md bg-green-500 px-4 py-2.5"
+        onClick={() => {
+          call.join();
+          setIsSetupComplete(true);
+        }}
+      >
+        Join meeting
+      </Button>
+    </div>
+  );
 }
